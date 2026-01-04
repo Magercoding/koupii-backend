41bb802f88617f5131dfdae219939e16ae4de458
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2026-01-04 20:15:04 +0700

    Add ScrambelServie providers

---
+++ app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Gate;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use App\Models\User;
+
+class ScrambleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
+        });
+
+        Gate::define('viewApiDocs', function () {
+            return true;
+        });
+
+        Scramble::registerApi('v1', ['info' => ['version' => '1.0']])
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            })
+            ->afterOpenApiGenerated(function (OpenApi $openApi) {
+                // Some operations on the resulting documentation.
+            });
+    }
+}
